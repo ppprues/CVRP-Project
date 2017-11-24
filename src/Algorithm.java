db15947b5fa2d15c7673f1ab0f147271427ea2c3
DESKTOP-058G3AO\Junior <junior_travian@hotmail.co.th>
2017-11-24 16:09:00 +0700

    pseudomain

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -5,6 +5,26 @@ public class Algorithm
 
         DataReader.initializeData();
 
+        //random gen0 arraylist of 60cell int array
+        //pick best
+        while(true)
+        {
+            //mutate  ->result = arraylist of 60int array
+            if(true) //if best result is the same
+            {
+                //countsame++;
+                if(true)//count same>10
+                {
+                    break;
+                    //break
+                    //best result is optimal
+                }
+            }
+        }
+        //pick1
+
+        //mutate
+
         System.out.println("Heeloo World");
     }
 }
