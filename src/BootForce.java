4677af681b22dbe6b06093e3e2b84eeddebde831
poo1882 <poo_poo1882@hotmail.com>
2017-11-28 18:21:34 +0700

    nothing

--- src/BootForce.java
+++ src/BootForce.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+public class BootForce
+{
+    private double minCost = 0;
+    public static void main (String[] args)
+    {
+        Location.calGeneCollection(10);
 
-public class BootForce {
+    }
 }
